989552bdf800bafd17750dd69d6be654fcf9a652
Felipe Augusto <98824060+FelipeABG@users.noreply.github.com>
2023-07-16 18:36:01 -0300

    v0.2
    added calculator display and reworked footer

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const calculator = {
+    display: document.querySelector('.display'),
+
+    renderDisplay(){
+        const displayButtons = document.querySelectorAll('.number-button, .operation-button')
+        displayButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                calculator.display.innerHTML += button.innerHTML
+            })
+        })
+    },
+
+    clearDisplay(){
+        const clearButtons = document.querySelectorAll('.clear-button')
+        clearButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                button.innerHTML === 'AC'? calculator.display.innerHTML = '': calculator.display.innerHTML = calculator.display.innerHTML.slice(0,-1)
+            })
+        })
+    },
+    
+
+}
+
+main()
+
+function main(){
+    calculator.renderDisplay()
+    calculator.clearDisplay()
+}
+
 
